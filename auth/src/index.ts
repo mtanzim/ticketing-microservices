5ffdf789ec2bf92214e8c78bfdd8bafda630de7a
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-04 16:33:32 -0500

    not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -12,6 +13,11 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 const PORT = 3000;
 app.listen(PORT, () => {
