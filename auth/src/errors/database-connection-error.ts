507986abc47172df5fe9c623163a17520e792389
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-04 16:05:20 -0500

    error normalization

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,5 +1,5 @@
 export class DatabaseConnectionError extends Error {
-  reasons = ["Error connecting to database"];
+  reasons = "Error connecting to database";
   constructor() {
     super();
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -2,28 +2,44 @@ import { NextFunction, Request, Response } from "express";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { RequestValidationError } from "../errors/request-validation";
 
+type ErrorResponse = {
+  errors: Array<{ message: string; field?: string }>;
+};
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const ERR_STATUS_CODE = 400;
+
   if (err instanceof RequestValidationError) {
     console.log("handling request error");
-    return res.status(400).send({
-      message: err.message,
-      reasons: err.errors,
-    });
+    const formattedErrors = err.errors.map((e) => ({
+      message: e.msg,
+      field: e.param,
+    }));
+    const errRes: ErrorResponse = { errors: formattedErrors };
+    return res.status(ERR_STATUS_CODE).send(errRes);
   }
   if (err instanceof DatabaseConnectionError) {
     console.log("handling db connection error error");
-    return res.status(400).send({
-      message: err.message,
-      reasons: err.reasons,
-    });
+    const errRes: ErrorResponse = {
+      errors: [
+        {
+          message: err.reasons,
+        },
+      ],
+    };
+    return res.status(ERR_STATUS_CODE).send(errRes);
   }
-  return res.status(400).send({
-    message: "Something went wrong",
-    reasons: [],
-  });
+
+  const defaultError: ErrorResponse = {
+    errors: [
+      {
+        message: "Something went wrong",
+      },
+    ],
+  };
+  return res.status(ERR_STATUS_CODE).send(defaultError);
 };
