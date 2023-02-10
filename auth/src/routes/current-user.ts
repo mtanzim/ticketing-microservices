7a76444d5bd2bbdaa7ee1a1644a8ac28b38f8cdf
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-10 18:04:56 -0500

    set up current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
+
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there buddy!");
+  if (!req.session?.jwt) {
+    return res.json({ currentUser: null });
+  }
+  const secret = process.env?.["JWT_KEY"];
+  try {
+    const payload = jwt.verify(req.session.jwt, secret!);
+    return res.json({ currentUser: payload });
+  } catch (err: any) {
+    throw new BadRequestError(err?.message || "Something went wrong");
+  }
 });
 
 export { router as currentUserRouter };
