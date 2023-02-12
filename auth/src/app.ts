c92e3070192eaea08be33af216cc05ab0d7d49c5
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-12 12:06:19 -0500

    move app to its own file

---
+++ auth/src/app.ts
@@ -0,0 +1,30 @@
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import express from "express";
+import "express-async-errors";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middleware/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
+export const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
