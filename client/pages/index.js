4cd3b0740ade0ebcbd349dac3a18f4fbd865dd89
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-14 18:01:39 -0500

    cp: econnrefused in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,18 @@
-export default function Root() {
-  return <h1>Landing page</h1>;
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get("/api/users/currentuser").catch((err) => {
+    console.log(err.message);
+  });
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  console.log("on the server");
+  const response = await axios.get("/api/users/currentuser")
+  console.log(response);
+  return response?.data;
+};
+export default LandingPage;
