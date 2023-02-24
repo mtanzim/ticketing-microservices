d24f0564ef3561fdfd1f3114a1c589e0d30a1ce6
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-24 10:27:55 -0500

    refactor publisher on my own

--- nats-example/src/publisher.ts
+++ nats-example/src/publisher.ts
@@ -1,21 +1,21 @@
 import nats from "node-nats-streaming";
+import { Subjects } from "./events/subjects";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
-const TOPIC = "ticket:created";
+const TOPIC = Subjects.TicketCreated;
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
-  console.log("connected");
   const data = {
     id: "123",
     title: "concert",
     price: 20,
   };
-  stan.publish(TOPIC, JSON.stringify(data), () => {
-    console.log("event published");
-  });
+  console.log("connected");
+  new TicketCreatedPublisher(stan).publish(data);
 });
