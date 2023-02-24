9f338690a431e50a0685d1d40fa721d7859fe980
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-24 10:28:37 -0500

    .

--- nats-example/src/publisher.ts
+++ nats-example/src/publisher.ts
@@ -1,10 +1,7 @@
 import nats from "node-nats-streaming";
-import { Subjects } from "./events/subjects";
 import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
-
-const TOPIC = Subjects.TicketCreated;
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
