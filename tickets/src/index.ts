a426f59dc25985ee192eb8ba13875a00df8b8f40
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-25 14:34:00 -0500

    graceful handling of nats crashing

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import { natsWrapper } from "./nats-wrapper";
 import { app } from "./app";
-
+import { randomBytes } from "crypto";
 const start = async () => {
   const clusterId = process.env.CLUSTER_ID;
-  const clientId = process.env.CLIENT_ID;
   const natsURL = process.env.NATS_URL;
-  if (!clusterId || !clientId || !natsURL) {
+  if (!clusterId || !natsURL) {
     throw new Error("CLIENT_ID and CLUSTER_ID must be defined for NATS");
   }
   if (!process.env.JWT_KEY) {
@@ -17,7 +16,22 @@ const start = async () => {
   }
 
   try {
-    await natsWrapper.connect(clusterId, clientId, natsURL);
+    await natsWrapper.connect(
+      clusterId,
+      randomBytes(4).toString("hex"),
+      natsURL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed.");
+      process.exit();
+    });
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to MongoDb");
   } catch (err) {
@@ -28,5 +42,8 @@ const start = async () => {
     console.log("tickets listening on port 3000");
   });
 };
+
+process.on("SIGINT", () => natsWrapper.client.close());
+process.on("SIGTERM", () => natsWrapper.client.close());
 
 start();
--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -2,7 +2,11 @@ import nats, { Stan } from "node-nats-streaming";
 
 class NatsWrapper {
   private _client?: Stan;
-  async connect(clusterId: string, clientId: string, url: string): Promise<void> {
+  async connect(
+    clusterId: string,
+    clientId: string,
+    url: string
+  ): Promise<void> {
     this._client = nats.connect(clusterId, clientId, { url });
 
     return new Promise((resolve, reject) => {
@@ -15,7 +19,7 @@ class NatsWrapper {
       });
     });
   }
-  client(): Stan | never {
+  get client(): Stan | never {
     if (!this._client) {
       throw new Error("nats client was not initialized");
     }
@@ -23,23 +27,3 @@ class NatsWrapper {
   }
 }
 export const natsWrapper = new NatsWrapper();
-
-// let stan: nats.Stan;
-
-// export function getStan(): nats.Stan | never {
-//   if (!stan) {
-//     throw new Error("NATS was not initialized!");
-//   }
-//   return stan;
-// }
-
-// export function startNATS() {
-
-//   stan = nats.connect(clusterId, clientId, {
-//     url: natsURL,
-//   });
-
-//   stan.on("connect", async () => {
-//     console.log("nats connected");
-//   });
-// }
