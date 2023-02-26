d3228789a3cad3b78110508d7a66e1cac5b73199
Tanzim Mokammel <mtanzim@gmail.com>
2023-02-25 21:08:46 -0500

    ticket updated publisher

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   UnauthorizedError,
 } from "@tm-tickets-1989/common";
 import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -36,6 +38,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
