d261585888d64a8286df6a6a5886d9b234ad28a7
Tanzim Mokammel <mtanzim@gmail.com>
2023-03-03 14:38:24 -0500

    new handler for orders

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,19 @@
-import { requireAuth, validateRequest } from "@tm-tickets-1989/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@tm-tickets-1989/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { natsWrapper } from "../nats-wrapper";
 import mongoose from "mongoose";
+import { Order } from "../models/order";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -13,28 +22,39 @@ router.post(
     body("ticketId")
       .not()
       .isEmpty()
-      .withMessage("ticketId is required")
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage("not a valid mongo id"),
+      .withMessage("ticketId must be provided"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { title, price } = req.body;
+    const { ticketId, userId } = req.body;
+    // find ticket
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
-    const ticket = Ticket.build({
-      title,
-      price,
+    // ensure ticket is not reserved
+    const isTicketReserved = await ticket.isReserved();
+    if (isTicketReserved) {
+      throw new BadRequestError("ticket is already reserved");
+    }
+    // generate an expiration date
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // build order and save to database
+    const order = Order.build({
+      expiresAt: expiration,
+      status: OrderStatus.Created,
+      ticket: ticket,
       userId: req.currentUser!.id,
     });
-    await ticket.save();
-    new TicketCreatedPublisher(natsWrapper.client).publish({
-      id: ticket.id,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-    });
+    await order.save();
+    // send an event that order was created
+    // TODO
 
-    res.status(201).send(ticket);
+    res.status(201).send(order);
   }
 );
 
